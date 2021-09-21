23b9f27bd590cda8cac1c6708058bda32ca6eec1
Mohamad Salahia <mhmtslahyt68@gmail.com>
2021-09-21 18:48:14 +0300

    updates template dashboard

--- resources/views/layouts/admin/_footer.blade.php
+++
@@ -1,3 +0,0 @@
-<footer class="main-footer">
-    <strong>CopyLeft &copy; 2020 <a href="https://qualitey.com/">كواليتي</a>.</strong>
-</footer>
--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,11 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::resource('carts', 'App\Http\Controllers\CartController')->except(['show']);
-
-//Route::get('/', 'App\Http\Controllers\CartController@home')->name('home');
-
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
